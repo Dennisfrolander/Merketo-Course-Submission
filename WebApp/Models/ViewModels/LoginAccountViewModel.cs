3e1ccb1a0a0dcd0b5d8c568ca2148fc62d89bb67
Dennisfrolander <111590730+Dennisfrolander@users.noreply.github.com>
2023-04-18 14:31:53 +0200

    Added LoginForm and RegisterAccount form

--- WebApp/Models/ViewModels/LoginAccountViewModel.cs
+++ WebApp/Models/ViewModels/LoginAccountViewModel.cs
@@ -1,5 +1,18 @@
-﻿namespace WebApp.Models.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.ViewModels;
 
 public class LoginAccountViewModel
 {
+	[Required(ErrorMessage = "You need to provide an e-mail adress")]
+	[DataType(DataType.EmailAddress)]
+	[Display(Name = "E-mail*")]
+	public string Email { get; set; } = null!;
+
+	[Required(ErrorMessage = "You need to provide a password")]
+	[DataType(DataType.Password)]
+	[Display(Name = "Password*")]
+	public string Password { get; set; } = null!;
+
+	public bool RememberMe { get; set; }
 }
